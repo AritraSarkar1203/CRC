1281f56ed9591bebe02d6dc9c3faf2a8d20c962d
Shivam Raj <email.shivamraj@gmail.com>
2024-12-13 20:30:49 +0530

    Final Change -3

--- Routes/bookings.js
+++ Routes/bookings.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { getBookings, createBooking , deleteBooking } from '../Controllers/bookingController.js';
+import { getBookings, createBooking , deleteBooking , statusBooking} from '../Controllers/bookingController.js';
 import { authenticateToken } from '../middleware/auth.js';
 
 const router = express.Router();
